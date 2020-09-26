0675bbe532cd16489c72618c543c58ecc428f94f
Josh Come Ibad <joshcibad@gmail.com>
2020-09-26 11:53:06 -0700

    Update LoginView.java

--- src/main/java/com/jkjprojektgruppe/schediastis/views/login/LoginView.java
+++ src/main/java/com/jkjprojektgruppe/schediastis/views/login/LoginView.java
@@ -1,5 +1,6 @@
 package com.jkjprojektgruppe.schediastis.views.login;
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
@@ -7,6 +8,8 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.jkjprojektgruppe.schediastis.views.main.MainView;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.router.RouterLink;
 
 @Route(value = "login", layout = MainView.class)
 @PageTitle("Login")
@@ -17,6 +20,15 @@ public class LoginView extends Div {
     public LoginView() {
         setId("login-view");
         add(new Label("Content placeholder"));
+        Button loginButton = new Button("Login");
+        loginButton.addClickListener(this::redirectToMain);
+        add(loginButton);
+    }
+
+    private void redirectToMain(ClickEvent<Button> buttonClickEvent) {
+        //Get Credentials from class-scope textboxes
+        //Check credentials against database
+        RouterLink link = new RouterLink("Main", MainView.class);
     }
 
 }
