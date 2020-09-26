4d3d97be1f2ece4bc18c06d66f43f485b8da760f
Josh Come Ibad <joshcibad@gmail.com>
2020-09-26 11:00:20 -0700

    Create LoginView.java

---
+++ src/main/java/com/jkjprojektgruppe/schediastis/views/login/LoginView.java
@@ -0,0 +1,20 @@
+package com.jkjprojektgruppe.schediastis.views.login;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.jkjprojektgruppe.schediastis.views.main.MainView;
+
+@Route(value = "login", layout = MainView.class)
+@PageTitle("Login")
+@CssImport("./styles/views/about/about-view.css")
+public class LoginView extends Div {
+
+    public LoginView() {
+        setId("login-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
